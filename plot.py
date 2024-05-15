db37f07e3d1896b6d7da574bf6d24b3ca6eca6fa
Eero Lehtinen <eero@eerolehtinen.fi>
2024-05-16 00:05:38 +0300

    Add combined plot

--- plot.py
+++ plot.py
@@ -4,7 +4,65 @@ import numpy as np
 import sys
 
 
-def main(name):
+NAMES = [
+    "distance_func",
+    "move_forces",
+    "micro_optimizations",
+    "spatial",
+    "parallel",
+]
+
+
+def combined():
+    level = "2-Labyrinth"
+    data = {}
+    for name in NAMES:
+        with open(f"statistics-{name}.json") as f:
+            stats = json.load(f)
+
+        for feature in stats:
+            data[feature] = []
+            x = np.array(stats[feature]["movement"][level]) * 1000
+            data[feature] = np.mean(x)
+
+    move_baseline = "spatial_array,distance_func2,new_movement,new_move_clamp"
+    spatial_baseline = (
+        "distance_func2,branchless,floatneighbors,new_movement,new_move_clamp"
+    )
+    legend_names = {
+        "spatial_array": "Baseline",
+        "spatial_array,distance_func2": "Quadratic distance",
+        "spatial_array,distance_func2,new_movement": "Low naviagation force",
+        "spatial_array,distance_func2,new_movement,new_move_clamp": "Low naviagation force & fixed clamping",
+        move_baseline + ",branchless": "Branchless",
+        move_baseline
+        + ",branchless,floatneighbors": "Branchless & float neighbors / Spatial array",
+        spatial_baseline + ",spatial_hash": "Spatial hash (ahash)",
+        spatial_baseline + ",spatial_hash_std": "Spatial hash (std hash)",
+        spatial_baseline + ",spatial_kdtree": "KD-Tree",
+        spatial_baseline + ",spatial_kdbush": "KD-Bush",
+        spatial_baseline + ",spatial_rstar": "R-Star",
+        spatial_baseline + ",parallel": "Parallel 8 cores, 16 threads",
+    }
+
+    print(data.keys())
+    print(legend_names.keys())
+
+    data = {k: v for k, v in data.items() if k in legend_names}
+
+    fig, ax = plt.subplots()
+
+    for key in reversed(data.keys()):
+        rect = ax.barh(legend_names[key], data[key])
+        plt.bar_label(rect, fmt="%.2f", padding=3)
+
+    ax.set_xlabel("Mean time (ms)")
+
+    plt.subplots_adjust(left=0.3, right=0.95)
+    plt.show()
+
+
+def plot(name):
     with open(f"statistics-{name}.json") as f:
         stats = json.load(f)
 
@@ -84,16 +142,12 @@ if __name__ == "__main__":
     else:
         arg = sys.argv[1]
 
-    NAME = [
-        "distance_func",
-        "move_forces",
-        "micro_optimizations",
-        "spatial",
-        "parallel",
-    ]
+    if arg == "combined":
+        combined()
+        sys.exit(0)
 
-    if arg not in NAME:
+    if arg not in NAMES:
         print(f"Name {arg} not found in NAMES")
         sys.exit(1)
 
-    main(arg)
+    plot(arg)
